53d7a82d66055a3102b997588e592e94afe2a575
Raphael Kuschmierz <fiffykuschmierz@gmail.com>
2024-02-02 12:51:02 +0100

    Update Fetching.jsx

--- soundfetcher/src/Fetching.jsx
+++ soundfetcher/src/Fetching.jsx
@@ -1,5 +1,6 @@
 async function fetchEAN(ean) {
     const data = await fetch('http://redreaperlp.de/api/ean/' + ean)
+    const data = await fetch('/api/ean/' + ean)
         .then(response => response.json())
         .catch(error => console.error(error));
     console.log(data.tracks[0].artists);
